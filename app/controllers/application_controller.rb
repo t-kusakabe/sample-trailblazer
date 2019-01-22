d83ff0d2eb49eb237beef4a49beb7493c632dad6
t-kusakabe <ttmac0704@gmail.com>
2019-01-22 16:04:17 +0900

    Create validate error message method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def translate_validation_errors(result)
+    validation_errors = result['validation_errors']
+    class_name = result['contract'].instance_variable_get(:@model).class.name
+    error_messages = []
+    validation_errors.each do |key, values|
+      values.each do |value|
+        error_messages << I18n.t('activerecord.attributes.' << class_name.downcase << '.' << key.to_s) + value
+      end
+    end
+    error_messages
+  end
 end

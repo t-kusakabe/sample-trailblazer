e31d5379d099a42324bd303b0e00137f96e69ba1
t-kusakabe <ttmac0704@gmail.com>
2019-01-22 18:50:28 +0900

    Create teacher operation destroy

---
+++ app/concepts/teachers/operation/destroy.rb
@@ -0,0 +1,23 @@
+class Teachers::Operation::Destroy < Applicatioon
+  step :validate
+  failure :handle_validation_error, fail_fast: true
+  step :presist!
+  failure :handle_internal_error!
+
+  private
+
+  def validate(options, params)
+    contract = Teachers::Contract::Destroy.new(Teacher.new)
+    contract.id = params[:id]
+    options['contract'] = contract
+    contract.valid?
+  end
+
+  def persist!(options, **)
+    contract = options['contract']
+    model = Teacher.find(contract.id)
+
+    options['model'] = model
+    model.destroy!
+  end
+end
